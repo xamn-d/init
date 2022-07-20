f7da59f4ed4b920e11bf94b76a506a358143be48
xamn-d <cam.dale15@gmail.com>
2022-07-20 04:30:20 -0500

    init.sh revised

--- packages/deb/init.sh
+++ packages/deb/init.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
 #update
+sudo apt-get update && sudo apt-get upgrade -y
 
 #clean
+sudo apt autoclean -y && sudo apt autoremove -y
 
 #depends
+##
 
 #desired
+##
 
 sudo reboot
--- packages/fed/init.sh
+++ packages/fed/init.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
 #update
+sudo dnf distro-sync -y && sudo sudo dnf upgrade -y && sudo yum distro-sync -y && sudo yum upgrade -y
 
 #clean
+sudo dnf autoremove -y && sudo yum autoremove -y
 
 #depends
+##
 
 #desired
+##
 
 sudo reboot
